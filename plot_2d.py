b6307c5ee92bd525f6460eb8bb085b5c3c686886
Stefan Hertel <stefan.hertel@gmail.com>
2021-06-06 20:30:19 +0000

    Initial version to be worked on

---
+++ plot_2d.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from matplotlib.ticker import NullFormatter
+
+nullfmt = NullFormatter()
+
+# change for talk or publication
+# mpl.rcParams.update({'font.size': 10})
+# rc('font', size='20')
+
